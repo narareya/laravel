1d1491ab077723d31cd96975ea1b6c2e9eb487dd
narareya <nomnomhiranami@gmail.com>
2025-10-09 07:16:06 +0700

    update controller

--- laravel-project/app/Http/Controllers/StudentController.php
+++ laravel-project/app/Http/Controllers/StudentController.php
@@ -49,3 +49,60 @@ class StudentController extends Controller
         ]);
     }
 }
+
+// one to many
+// public function students() {
+//     return $this->hasMany(Student::class, 'classroom_id');
+// }
+
+// public function classroom() {
+//     return $this->belongsTo(Classroom::class, 'classroom_id');
+// }
+
+// // ambil semua siswa di kelas A
+// $classroom = Classroom::find(1);
+// $students = $classroom->students;
+
+// // ambil kelas dari seorang siswa
+// $student = Student::find(5);
+// $class = $student->classroom;
+
+
+
+// one to one
+// public function profile() {
+//     return $this->hasOne(Profile::class);
+// }
+
+// public function student() {
+//     return $this->belongsTo(Student::class);
+// }
+
+// $student = Student::find(1);
+// $profile = $student->profile;
+
+// $profile = Profile::find(1);
+// $student = $profile->student;
+
+
+
+// many to many
+// contoh L Student.php
+// public function clubs() {
+//     return $this->belongsToMany(Club::class, 'club_student', 'student_id', 'club_id');
+// }
+
+// contoh : Club.php
+// public function students() {
+//     return $this->belongsToMany(Student::class, 'club_student', 'club_id', 'student_id');
+// }
+
+// controller
+// $student = Student::find(1);
+// $student->clubs; // semua club yang diikutin
+
+// $club = Club::find(2);
+// $club->students; // semua siswa yang ikut club itu
+
+// // nambahin hubungan baru
+// $student->clubs()->attach(2);
